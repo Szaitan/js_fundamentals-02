55d9dcda4b6ba456d4d90842bc2c017c36c19a72
Michal <m.krepiniewicz@gmail.com>
2024-10-18 17:24:50 +0200

    Arrays and objects

--- assigments.js
+++ assigments.js
@@ -13,29 +13,78 @@ function percentageOfWorld1(population) {
     return (population * 100) / worldPopulation
 }
 
-chinaPercentage = percentageOfWorld1(1441)
-polandPercentage = percentageOfWorld1(200)
-indiaPercentage =  percentageOfWorld1(1600)
-console.log(chinaPercentage, polandPercentage, indiaPercentage)
+// chinaPercentage = percentageOfWorld1(1441)
+// polandPercentage = percentageOfWorld1(200)
+// indiaPercentage = percentageOfWorld1(1600)
+// console.log(chinaPercentage, polandPercentage, indiaPercentage)
 
-// function expression
-const percentageOfWorld2 = function (population) {
-    return (population * 100) / worldPopulation
+// // function expression
+// const percentageOfWorld2 = function (population) {
+//     return (population * 100) / worldPopulation
+// }
+
+// chinaPercentage2 = percentageOfWorld2(1441)
+// polandPercentage2 = percentageOfWorld2(200)
+// indiaPercentage2 = percentageOfWorld2(1600)
+// console.log(chinaPercentage2, polandPercentage2, indiaPercentage2)
+
+// // Assigment 3
+// // function arrow
+// const percentageOfWorld3 = (population) => (population * 100) / worldPopulation
+
+// console.log(percentageOfWorld3(1441))
+
+// // Assigment 4
+// const describePopulation = function (country, population) {
+//     return `${country} has ${population} population, which is about ${percentageOfWorld1(population)} of the world.\n`
+// }
+// console.log(describePopulation("China", 1441), describePopulation("Poland", 200), describePopulation("India", 1600))
+
+// Assigment 5
+
+// const countryList = ["Poland", "Englad", "France", "Czech"]
+// console.log(countryList.length === 4)
+
+// const percentages = [1441, 200, 1600, 340]
+// console.log(percentageOfWorld1(percentages[0]), percentageOfWorld1(percentages[1]), percentageOfWorld1(percentages[2]), percentageOfWorld1(percentages[3]))
+
+// Assigment 6
+// const neighbours = ["Mexic", "Canda"]
+// neighbours.push("Utopia")
+// console.log(neighbours)
+// neighbours.pop()
+// console.log(neighbours)
+
+// if (!neighbours.includes("Germany")) console.log("Probably not a central european country :D")
+
+// const mexicIndex = neighbours.indexOf("Mexic")
+// neighbours[mexicIndex] = "Sweden"
+
+// console.log(neighbours)
+
+// Challenge
+// const jonas = {
+//     firstName: "Jonas",
+//     lastName: "Schmedtmann",
+//     age: 2037 - 1991,
+//     job: "teacher",
+//     friends: ['Micheal', "Peter", "Steven"]
+// }
+
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
+
+// Assigment 7
+const mycoutry = {
+    country: "Germany",
+    capital: "Berlin",
+    language: "Deutsch",
+    population: 300,
+    neighbours: ["Poland", "Czech", "Belgium", "Netherlands"]
 }
 
-chinaPercentage2 = percentageOfWorld2(1441)
-polandPercentage2 = percentageOfWorld2(200)
-indiaPercentage2 =  percentageOfWorld2(1600)
-console.log(chinaPercentage2, polandPercentage2, indiaPercentage2)
+console.log(`'${mycoutry.country} has ${mycoutry.population} million ${mycoutry.language}-speaking people, ${mycoutry.neighbours.length} neighbouring countries and a capital called ${mycoutry.capital}'`)
+mycoutry.population += 2
+console.log(mycoutry.population)
 
-// Assigment 3
-// function arrow
-const percentageOfWorld3 = (population) => (population * 100) / worldPopulation
-
-console.log(percentageOfWorld3(1441))
-
-// Assigment 4
-const describePopulation = function (country , population) {
-    return `${country} has ${population} population, which is about ${percentageOfWorld1(population)} of the world.\n`
-}
-console.log(describePopulation("China", 1441), describePopulation("Poland", 200), describePopulation("India", 1600))
+mycoutry["population"] -= 2
+console.log(mycoutry.population)
